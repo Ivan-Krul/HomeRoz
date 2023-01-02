a47e750d23ea9fcf9cf81c3abb3da1bd67bc2e2b
Ivan-Krul <kiwiivanya128@gmail.com>
2023-01-02 15:40:21 +0200

    making token syntax in queryhomeroz class

--- QueryHomeRoz/queryhomeroz.cpp
+++ QueryHomeRoz/queryhomeroz.cpp
@@ -1,22 +1,72 @@
 #include "queryhomeroz.h"
 
+TokenIndex QueryHomeRoz::SearchToken_(std::string str)
+{
+	for (size_t i = 0; i < cs_list_of_token_.size(); i++)
+		if (cs_list_of_token_[i] == str)
+			return static_cast<TokenIndex>(i);
+	return TokenIndex::nothing;
+}
+
 std::string QueryHomeRoz::Query(std::string query)
 {
-    std::string output;
-    auto separated = additional_string_lib::SplitStr(query);
-    if (separated == "SIZE")
-    {
-        separated = additional_string_lib::SplitStr(query);
-        if (separated == "OF")
-        {
-            separated = additional_string_lib::SplitStr(query);
-            if (separated == "lessons")
-            {
-                output = std::to_string(mgr_.SizeLessons());
-                goto Answer;
-            }
-        }
-    }
-    Answer:
-    return output;
+	std::string output;
+	auto separated = additional_string_lib::SplitStr(query);
+	TokenIndex token_ind;
+	if (!size_t(token_ind = SearchToken_(separated)))
+	{
+		output = "INVALID QUERY TOKEN";
+		goto Answer;
+	}
+	switch (token_ind)
+	{
+		case TokenIndex::nothing:
+			break;
+		case TokenIndex::size_of:
+			separated = additional_string_lib::SplitStr(query);
+			switch (token_ind = SearchToken_(separated))
+			{
+				case TokenIndex::lesson:
+					break;
+				case TokenIndex::homework:
+					break;
+				case TokenIndex::size_of_in:
+					break;
+				default:
+					break;
+			}
+			break;
+		case TokenIndex::output:
+			separated = additional_string_lib::SplitStr(query);
+			break;
+		case TokenIndex::set:
+			separated = additional_string_lib::SplitStr(query);
+			break;
+		case TokenIndex::clear:
+			mgr_.Clear();
+			break;
+		case TokenIndex::create:
+			separated = additional_string_lib::SplitStr(query);
+			break;
+		case TokenIndex::erase:
+			separated = additional_string_lib::SplitStr(query);
+			break;
+		default:
+			goto NotValid;
+			break;
+	}
+	if (token_ind == TokenIndex::nothing
+		|| token_ind == TokenIndex::set
+		|| token_ind == TokenIndex::clear
+		|| token_ind == TokenIndex::create
+		|| token_ind == TokenIndex::erase)
+	{
+		output = "SUCCESS";
+		goto Answer;
+	}
+NotValid:
+	output = "QUERY TOKEN EXIST BUT NOT VALID";
+	goto Answer;
+Answer:
+	return output;
 }
--- QueryHomeRoz/queryhomeroz.h
+++ QueryHomeRoz/queryhomeroz.h
@@ -7,27 +7,62 @@
 
 class QueryHomeRoz
 {
-	const static std::vector<std::string> cs_list_of_token_ =
-	{
-		//UNDONE: fill this list of tokens
-		// Definitions
-		"LESSON",
-		"HOMEWORK",
-		// Size
-		"SIZE OF", // () -> LESSON | HOMEWORK
-		"IN", // SIZE OF HOMEWORK -> () -> lesson_name
-		// Get
-		"OUTPUT", // () -> LESSON | HOMEWORK
-		// Set something
-		"SET", // () -> LESSON | HOMEWORK
-		"NAME",
-		"LINK",
-		"WEEK",
+	static const std::vector<std::string> cs_list_of_token_;
 
-		"CONTEXT",
-	};
 	HomeRozMGR mgr_;
+private:
+	TokenIndex SearchToken_(std::string str);
 public:
 	std::string Query(std::string query);
 };
 
+enum class TokenIndex
+{
+	nothing = 0,
+	lesson,
+	homework,
+	size_of,
+	size_of_in,
+	output,
+	set,
+	set_name,
+	set_link,
+	set_week,
+	set_week_not,
+	set_context,
+	set_date,
+	set_date_creation,
+	set_date_term,
+	set_done,
+	clear,
+	create,
+	erase
+};
+
+const std::vector<std::string> QueryHomeRoz::cs_list_of_token_ =
+{
+	//UNDONE: fill this list of tokens
+	// Definitions
+	"NOTHING",
+	"LESSON",
+	"HOMEWORK",
+		// Size
+	"SIZE OF", // () -> LESSON | HOMEWORK
+	"IN", // SIZE OF HOMEWORK -> () -> lesson_name
+		// Get
+	"OUTPUT", // () -> LESSON | HOMEWORK
+		// Set something
+	"SET", // () -> LESSON | HOMEWORK
+	"NAME", // SET LESSON lesson_name -> () -> ...
+	"LINK",
+	"WEEK", // SET LESSON lesson_name -> () -> NOT | week (3 letters)
+	"NOT",
+	"CONTEXT",
+	"DATE",
+	"CREATION",
+	"TERM TO",
+	"DONE",
+	"CLEAR",
+	"CREATE",
+	"ERASE"
+};
